6be48bc149af069e26c81ba3fca344a3beddc497
Devrath Rana <chintusinghrana1234567890@gmail.com>
2023-02-20 00:04:12 +0530

    fix

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -1,7 +1,7 @@
 export const User = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-2xl font-inter font-semibold leading-8 text-gray-900">
+      <h1 className="text-2xl font-inter font-semibold leading-8 text-gray-900 mt-4">
         User Management
       </h1>
     </div>
