4b21b9d92a672035d56e96034de3fdd0505a05d0
Devrath Rana <devrath.rana@kellton.com>
2023-04-03 10:57:58 +0530

    fix

--- src/utils/config/Config.js
+++ src/utils/config/Config.js
@@ -2,4 +2,5 @@ export const config = {
   appName: "ums_user",
   env: process.env.REACT_APP_ENV,
   baseUrl: process.env.REACT_APP_BASE_URL,
+  baseImagePath: process.env.REACT_APP_IMAGE_BASE_PATH
 };
